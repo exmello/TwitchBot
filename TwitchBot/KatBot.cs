741424d5ee10f54f56965d2ec01e299f2e05bf3d
Danny Walker <exmello@msn.com>
2016-03-15 01:32:28 -0400

    added new messages, testing in kat's channel

--- TwitchBot/KatBot.cs
+++ TwitchBot/KatBot.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TwitchBot
 {
     public class KatBot
     {
         private readonly TwitchResponseWriter tw;
+
+        private Regex regLettuce = new Regex("lettuce", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+        private Regex regKebab = new Regex("kebab|kabob", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+        private Regex regNips = new Regex("nips", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+        
         public KatBot(TwitchResponseWriter tw)
         {
             this.tw = tw;
 
             // Lets you know its working
-            tw.RespondMessage("BEEP BOOP");
-            Console.WriteLine("BEEP BOOP CHAT STARTING.\r\n\r\n.");
+            //tw.RespondMessage(": Activating MrDestructoid");
+            //Console.WriteLine("BEEP BOOP CHAT STARTING.\r\n\r\n.");
         }
 
         public void ProcessMessage(string username, string message)
         {
             if (!(username.Equals(Config.Nickname, StringComparison.InvariantCultureIgnoreCase) && message.StartsWith(Config.BotName, StringComparison.InvariantCultureIgnoreCase)))
             {
-                if (message.Contains("lettuce"))
+                if (regNips.IsMatch(message))
                 {
-                    tw.RespondMessage("yummy");
+                    tw.RespondMessage(": It's KatNIP. No 's'! BibleThump");
+                }
+                else if (regLettuce.IsMatch(message))
+                {
+                    tw.RespondMessage(": Did someone say LETTUCE? PogChamp");
+                }
+                else if (regKebab.IsMatch(message))
+                {
+                    tw.RespondMessage(": KEBAB? I prefer lettuce. FailFish");
                 }
             }
         }
